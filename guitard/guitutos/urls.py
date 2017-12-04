f4134c7be60e1168ec1972fa613ba79917741fd3
Baptiste Pouget <baba@firemail.cc>
2017-12-04 18:22:04 +0100

    ajout authentification

--- guitard/guitutos/urls.py
+++ guitard/guitutos/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^tutos/$', views.liste_tutos, name='liste_tutos'),
     url(r'^t/(?P<id_tuto>[0-9]+)/$', views.tuto, name='tuto'),
+    url(r'^inscription/$', views.inscription, name='inscription'),
+    url(r'^connexion/$', views.connexion, name='connexion'),
+    url(r'^deconnexion/$', views.deconnexion, name='deconnexion'),
 ]
--- guitard/guitutos/views.py
+++ guitard/guitutos/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from django.shortcuts import get_object_or_404, render
-
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 from .models import Tutoriel
 from .models import Commentaire
 
@@ -27,5 +29,34 @@ def tuto(request, id_tuto):
         'tutoriel': tutoriel,
     }
     return HttpResponse(template.render(context, request))
+
+def inscription(request):
+    template = loader.get_template('guitutos/inscription.html')
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('index')
+    else:
+        form = UserCreationForm()
+    return HttpResponse(template.render({'form':form}, request))
+
+def connexion(request):
+    template = loader.get_template('guitutos/connexion.html')
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+    return HttpResponse(template.render(None, request))
+    
+
+def deconnexion(request):
+    logout(request)
+    return redirect('index')
 
 # Create your views here.
