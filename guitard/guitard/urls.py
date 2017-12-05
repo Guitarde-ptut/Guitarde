b47d9c84af4b922525242d41727ee9f9619b7733
Baptiste Pouget <baba@firemail.cc>
2017-12-05 14:33:36 +0100

    ajout à la racine, et style

--- guitard/guitard/urls.py
+++ guitard/guitard/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^tutos/', include('guitutos.urls')),
+    url(r'^',  include('guitutos.urls')),
     url(r'^admin/', admin.site.urls),
 ]
